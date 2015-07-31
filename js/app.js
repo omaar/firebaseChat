243d7ff9ee7e446986de3b397fdef95564fa01c7
Omar Regino <omarjrz21@gmail.com>
2015-07-31 00:09:42 -0500

    Firebase Deploy

--- js/app.js
+++ js/app.js
@@ -1,21 +1,29 @@
 var firebase = new Firebase('https://chat-omaar.firebaseio.com/');
 		$('#message').keypress(function(e){
 			if (e.keyCode == 13) {
-				var name = $('#name').val();
-				var text = $('#message').val();
-				$('#message').text('');
-				firebase.push({
-					name: name, text:text
-				});
+				pushMessage();
 			}
 		});
 
-		firebase.on('child_added', function(snapshot){
-			var message = snapshot.val();
-			displayMessages(message.name, message.text);
-		});
+$('#send').bind('click', function() {
+	pushMessage();
+});
+function pushMessage(){
+	var name = $('#name').val();
+	var text = $('#message').val();
+	$('#message').val('');
+	firebase.push({
+		name: name, text:text
+	});
+return false;
+}
+firebase.on('child_added', function(snapshot){
+	var message = snapshot.val();
+	displayMessages(message.name, message.text);
+});
 
 function displayMessages(name, text){
 	$('<div>').text(text).prepend($('<span class="label label-info">').text(name + ': ')).appendTo($('#messages'));
 	$('#messages')[0].scrollTop = $('#messages')[0].scrollHeight;
+	return false;
 };
